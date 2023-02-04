44f2e577639d315dfb059a571aca8a36c2f484ae
Renan Siqueira <renan.siqueira8a@gmail.com>
2023-02-04 16:46:15 -0300

    node basics commit

---
+++ 01-node-basics/02-globals.js
@@ -0,0 +1,11 @@
+///GLOBALS
+//__dirname
+//__filename
+//require
+//module
+//process
+console.log(__dirname);
+
+setInterval(() => {
+    console.log('hellow world');
+}, 1000);
---
+++ 01-node-basics/2-async-patterns/1-block.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+    if(req.url === '/'){
+        res.end('Home Page');
+        return;
+    }
+    if(req.url === '/about'){
+        //BLOCKING CODE!!!
+        for (let i = 0; i < 50; i++) {
+            for (let j = 0; j < 1000; j++) {
+                console.log(`${i} ${j}`);
+            }     
+        }
+        res.end('About Page');
+        return;
+    }
+    res.end('Error Page');
+
+})
+
+server.listen(5000, () => {
+    console.log('Server listening on port 5000...');
+})
